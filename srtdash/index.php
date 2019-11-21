8f3bc8128bcc040a6405fcd6b522b3ea7b17a88a
zheuswalker <thndrwrth@gmail.com>
2019-11-21 13:58:54 +0800

    test login

--- srtdash/index.php
+++ srtdash/index.php
@@ -4,7 +4,7 @@ $errorMsg = "";
 $validUser = $_SESSION["login"] === true;
 if(isset($_POST["sub"])) {
   $validUser = $_POST["username"] == "admin" && $_POST["password"] == "password";
-  if(!$validUser) $errorMsg = "Invalid username or password.";
+  if(!$validUser) echo "Invalid username or password.";
   else $_SESSION["login"] = true;
 }
 if($validUser) {
